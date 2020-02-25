de28a882dedc1d3719ceab78b43e01133e36eb49
Abundiz <3duabundiz@gmail.com>
2020-02-25 06:53:41 -0600

    Scripts para obtener informacion de la BD

---
+++ scripts/fechas.php
@@ -0,0 +1,27 @@
+<?php
+    $servidor = "sistemas.cucei.udg.mx";
+    $usuario = "appcucei";
+    $contrasena = "s297MGabj33IWAY4";
+    $bd = "eventos_udg";
+
+    $conexion = mysqli_connect($servidor,$usuario, $contrasena ,$bd);    
+
+    //Eventos entre hoy y menos de 3 días ordenados por fecha
+    $seleccion = "SELECT * from eventos_udg WHERE(DATEDIFF(fecha, CURDATE()))<3 AND DATEDIFF(fecha, CURDATE())>-1 ORDER BY fecha";
+        
+    $consulta = mysqli_query($conexion,$seleccion) or die("error en la consulta");
+
+    $eventos;     //Arreglo con los eventos próximos    
+    while($row = mysqli_fetch_assoc($consulta)){
+        
+        $eventos[] = $row;        
+    }   
+
+    var_dump($eventos);
+    echo "<br>";
+    echo json_encode($eventos);
+    
+    mysqli_close($conexion);
+
+
+?>
